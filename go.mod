ff8dc5894e8cc3021c4e1a902aec09a707bde2d1
Chris <github.account@chrigel.net>
2022-04-12 15:24:34 +0200

    Bootstrap operator sub command

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vshn/appcat-service-postgresql
 go 1.17
 
 require (
+	github.com/ccremer/go-command-pipeline v0.15.0
 	github.com/crossplane/crossplane-runtime v0.15.1
 	github.com/crossplane/crossplane-tools v0.0.0-20220106150503-21c1d7761d68
 	github.com/go-logr/logr v1.2.3
@@ -11,6 +12,7 @@ require (
 	github.com/urfave/cli/v2 v2.4.0
 	go.uber.org/zap v1.21.0
 	k8s.io/apimachinery v0.23.5
+	k8s.io/client-go v0.23.5
 	sigs.k8s.io/controller-runtime v0.11.2
 	sigs.k8s.io/controller-tools v0.8.0
 	sigs.k8s.io/kind v0.12.0
@@ -22,28 +24,38 @@ require (
 	github.com/alecthomas/units v0.0.0-20210912230133-d1bdfacee922 // indirect
 	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/benbjohnson/clock v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/dave/jennifer v1.4.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gobuffalo/flect v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.28.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/afero v1.8.0 // indirect
 	github.com/spf13/cobra v1.2.1 // indirect
@@ -59,6 +71,7 @@ require (
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	golang.org/x/tools v0.1.6-0.20210820212750-d4cc65f0b2ff // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
@@ -67,7 +80,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/api v0.23.5 // indirect
 	k8s.io/apiextensions-apiserver v0.23.5 // indirect
-	k8s.io/client-go v0.23.5 // indirect
+	k8s.io/component-base v0.23.5 // indirect
 	k8s.io/klog/v2 v2.30.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
--- operator_command.go
+++ operator_command.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"os"
-	"sync"
+	"context"
+	"time"
 
+	pipeline "github.com/ccremer/go-command-pipeline"
+	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"github.com/urfave/cli/v2"
+	"github.com/vshn/appcat-service-postgresql/apis"
+	"github.com/vshn/appcat-service-postgresql/operator"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 type operatorCommand struct {
+	SyncInterval          time.Duration
+	LeaderElectionEnabled bool
+
+	manager    manager.Manager
+	kubeconfig *rest.Config
 }
 
 var operatorCommandName = "operator"
@@ -19,39 +33,75 @@ func newOperatorCommand() *cli.Command {
 		Usage:  "Start provider in operator mode",
 		Before: command.validate,
 		Action: command.execute,
+		Flags: []cli.Flag{
+			&cli.DurationFlag{Name: "sync-interval", Value: time.Hour, EnvVars: envVars("SYNC_INTERVAL"),
+				Usage:       "How often all resources will be double-checked for drift from the desired state.",
+				Destination: &command.SyncInterval,
+			},
+			&cli.BoolFlag{Name: "leader-election-enabled", Value: false, EnvVars: envVars("LEADER_ELECTION_ENABLED"),
+				Usage:       "Use leader election for the controller manager.",
+				Destination: &command.LeaderElectionEnabled,
+			},
+		},
 	}
 }
 
-func (c *operatorCommand) validate(context *cli.Context) error {
-	_ = LogMetadata(context)
-	log := AppLogger(context).WithName(operatorCommandName)
+func (c *operatorCommand) validate(ctx *cli.Context) error {
+	_ = LogMetadata(ctx)
+	log := AppLogger(ctx).WithName(operatorCommandName)
 	log.V(1).Info("validating config")
 	return nil
 }
 
-func (c *operatorCommand) execute(context *cli.Context) error {
-	log := AppLogger(context).WithName(operatorCommandName)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		// Shutdown hook. Can be used to gracefully shutdown listeners or pre-shutdown cleanup.
-		// Can be removed if not needed.
-		// Please note that this example is incomplete and doesn't cover all cases when properly implementing shutdowns.
-		defer wg.Done()
-		<-context.Done()
-		err := c.shutdown(context)
-		if err != nil {
-			log.Error(err, "cannot properly shut down")
-			os.Exit(2)
-		}
-	}()
-	log.Info("Hello from operator command!", "config", c)
-	wg.Wait()
-	return nil
-}
+func (c *operatorCommand) execute(ctx *cli.Context) error {
+	log := AppLogger(ctx).WithName(operatorCommandName)
+	log.Info("Setting up controllers", "config", c)
 
-func (c *operatorCommand) shutdown(context *cli.Context) error {
-	log := AppLogger(context).WithName(operatorCommandName)
-	log.Info("Shutting down operator command")
-	return nil
+	p := pipeline.NewPipeline().WithBeforeHooks([]pipeline.Listener{
+		func(step pipeline.Step) {
+			log.V(1).Info(step.Name)
+		},
+	})
+	p.AddStepFromFunc("get config", func(ctx context.Context) error {
+		cfg, err := ctrl.GetConfig()
+		c.kubeconfig = cfg
+		return err
+	})
+	p.AddStepFromFunc("create manager", func(ctx context.Context) error {
+		// configure client-side throttling
+		c.kubeconfig.QPS = 100
+		c.kubeconfig.Burst = 150 // more Openshift friendly
+
+		mgr, err := ctrl.NewManager(c.kubeconfig, ctrl.Options{
+			SyncPeriod: &c.SyncInterval,
+			// controller-runtime uses both ConfigMaps and Leases for leader election by default.
+			// Leases expire after 15 seconds, with a 10-second renewal deadline.
+			// We've observed leader loss due to renewal deadlines being exceeded when under high load - i.e.
+			//  hundreds of reconciles per second and ~200rps to the API server.
+			// Switching to Leases only and longer leases appears to alleviate this.
+			LeaderElection:             c.LeaderElectionEnabled,
+			LeaderElectionID:           "crossplane-leader-election-provider-appcat-postgresql",
+			LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+			LeaseDuration:              func() *time.Duration { d := 60 * time.Second; return &d }(),
+			RenewDeadline:              func() *time.Duration { d := 50 * time.Second; return &d }(),
+		})
+		c.manager = mgr
+		return err
+	})
+	p.AddStepFromFunc("register schemes", func(ctx context.Context) error {
+		return apis.AddToScheme(c.manager.GetScheme())
+	})
+	p.AddStepFromFunc("setup controllers", func(ctx context.Context) error {
+		o := controller.Options{
+			Log:         log,
+			RateLimiter: ratelimiter.NewDefaultProviderRateLimiter(1000),
+		}
+		return operator.Setup(c.manager, o)
+	})
+	p.AddStepFromFunc("run manager", func(ctx context.Context) error {
+		log.Info("Starting manager")
+		return c.manager.Start(ctx)
+	})
+	p.WithOptions(pipeline.DisableErrorWrapping)
+	return p.RunWithContext(ctx.Context).Err()
 }
