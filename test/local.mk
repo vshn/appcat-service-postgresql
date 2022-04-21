5e6bc72c78198a5dbe69ff5053daa0952bb989e6
Chris <github.account@chrigel.net>
2022-04-21 18:08:41 +0200

    Install webhook support in chart

--- test/local.mk
+++ test/local.mk
@@ -1,16 +1,40 @@
+webhook_key = $(kind_dir)/webhook.key
+webhook_cert = $(kind_dir)/webhook.crt
+webhook_service_name = provider-postgresql.postgresql-system.svc
+webhook_values = $(kind_dir)/webhook-values.yaml
+
+ifeq ($(shell uname -s),Darwin)
+	b64 := base64
+else
+  b64 := base64 -w0
+endif
+
 # Despite the registry running in Cluster, we need to load the container image with `kind load`.
 # There were problems trying to pull container image from registry ("no such host") even though Crossplane could pull the package image...
 .PHONY: package-install
 package-install: export KUBECONFIG = $(KIND_KUBECONFIG)
-package-install: kind-load-image install-crd ## Install Operator in local cluster
+package-install: kind-load-image install-crd webhook-cert ## Install Operator in local cluster
 	helm upgrade --install provider-postgresql chart \
-		--create-namespace --namespace crossplane-system \
+		--create-namespace --namespace postgresql-system \
 		--set "args[0]='--log-level=2" \
 		--set "args[1]='operator'" \
 		--set podAnnotations.date="$(shell date)" \
+		--values $(webhook_values) \
 		--wait
 
 .PHONY: kind-run-operator
 kind-run-operator: export KUBECONFIG = $(KIND_KUBECONFIG)
 kind-run-operator: kind-setup ## Run in Operator mode against kind cluster (you may also need `install-crd`)
 	go run . -v 1 operator
+
+.PHONY: webhook-cert
+webhook-cert: $(webhook_values)
+
+$(webhook_key):
+	openssl req -x509 -newkey rsa:4096 -nodes -keyout $@ --noout -days 3650 -subj "/CN=$(webhook_service_name)" -addext "subjectAltName = DNS:$(webhook_service_name)"
+
+$(webhook_cert): $(webhook_key)
+	openssl req -x509 -key $(webhook_key) -nodes -out $@ -days 3650 -subj "/CN=$(webhook_service_name)" -addext "subjectAltName = DNS:$(webhook_service_name)"
+
+$(webhook_values): $(webhook_cert)
+	@yq -n '.webhook.caBundle="$(shell $(b64) $(webhook_cert))" | .webhook.certificate="$(shell $(b64) $(webhook_cert))" | .webhook.privateKey="$(shell $(b64) $(webhook_key))"' > $(kind_dir)/webhook-values.yaml
