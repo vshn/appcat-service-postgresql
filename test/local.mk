b69cf9475137f18fc73f190004c176fb713b7576
Chris <github.account@chrigel.net>
2022-04-25 09:31:14 +0200

    Remove package-provider GH action

--- test/local.mk
+++ test/local.mk
@@ -11,9 +11,9 @@ endif
 
 # Despite the registry running in Cluster, we need to load the container image with `kind load`.
 # There were problems trying to pull container image from registry ("no such host") even though Crossplane could pull the package image...
-.PHONY: package-install
-package-install: export KUBECONFIG = $(KIND_KUBECONFIG)
-package-install: kind-load-image install-crd webhook-cert ## Install Operator in local cluster
+.PHONY: local-install
+local-install: export KUBECONFIG = $(KIND_KUBECONFIG)
+local-install: kind-load-image install-crd webhook-cert ## Install Operator in local cluster
 	helm upgrade --install provider-postgresql chart \
 		--create-namespace --namespace postgresql-system \
 		--set "args[0]='--log-level=2" \
