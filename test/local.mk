941fb813c013d2c12b4ee1f78edecf4d5c42a7a1
Chris <github.account@chrigel.net>
2022-05-03 13:12:52 +0200

    Add boilerplate for running integration tests with envtest

--- test/local.mk
+++ test/local.mk
@@ -1,3 +1,15 @@
+setup_envtest_bin = $(kind_dir)/setup-envtest
+
+# Prepare kind binary
+# We need to set the Go arch since the binary is meant for the user's OS.
+$(setup_envtest_bin): export GOOS = $(shell go env GOOS)
+$(setup_envtest_bin): export GOARCH = $(shell go env GOARCH)
+$(setup_envtest_bin):
+	@mkdir -p $(kind_dir)
+	cd test && go build -o $@ sigs.k8s.io/controller-runtime/tools/setup-envtest
+	$@ $(ENVTEST_ADDITIONAL_FLAGS) use '$(ENVTEST_K8S_VERSION)!'
+	chmod -R +w $(kind_dir)/k8s
+
 webhook_key = $(kind_dir)/tls.key
 webhook_cert = $(kind_dir)/tls.crt
 webhook_service_name = provider-postgresql.postgresql-system.svc
@@ -37,3 +49,9 @@ $(webhook_cert): $(webhook_key)
 
 $(webhook_values): $(webhook_cert)
 	@yq -n '.webhook.caBundle="$(shell $(b64) $(webhook_cert))" | .webhook.certificate="$(shell $(b64) $(webhook_cert))" | .webhook.privateKey="$(shell $(b64) $(webhook_key))"' > $(kind_dir)/webhook-values.yaml
+
+.PHONY: test-integration
+test-integration: export ENVTEST_CRD_DIR = $(shell realpath $(PROJECT_ROOT_DIR))/package/crds
+test-integration: $(setup_envtest_bin) ## Run integration tests against code
+	export KUBEBUILDER_ASSETS="$$($(setup_envtest_bin) $(ENVTEST_ADDITIONAL_FLAGS) use -i -p path '$(ENVTEST_K8S_VERSION)!')" && \
+	go test -tags=integration -coverprofile cover.out -covermode atomic ./...
